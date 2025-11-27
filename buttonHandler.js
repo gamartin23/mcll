282aaedae6f1f6004f3e8407a557ab91bff9139b
Kova <105208079+gamartin23@users.noreply.github.com>
2025-11-27 00:26:00 -0300

    ni idea ojalá llueva

--- buttonHandler.js
+++ buttonHandler.js
@@ -129,26 +129,6 @@ function sortGridItems(sortBy) {
                         stickyText.classList.add('sticky-text');
                         stickyText.innerText = `La concha de tu madre, ${parentId.toUpperCase()}!`;
 
-                        stickyText.style.position = 'fixed'; 
-                        stickyText.style.top = '50px'; 
-                        stickyText.style.left = '50%';    
-                        stickyText.style.transform = 'translateX(-50%)';
-                        stickyText.style.backgroundColor = 'rgb(0, 163, 68)'; 
-                        stickyText.style.color = '#ffffff'; 
-                        stickyText.style.padding = '10px 20px';
-                        stickyText.style.borderRadius = '8px'; 
-                        stickyText.style.zIndex = '9999';
-                        stickyText.style.fontSize = '30px'; 
-                        stickyText.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.3)'; 
-                        stickyText.style.textShadow = `
-                            -1px -1px 2px black,
-                            1px -1px 2px black,
-                            -1px 1px 2px black,
-                            1px 1px 2px black
-                        `;
-                        stickyText.style.opacity = '0';
-                        stickyText.style.transition = 'opacity 1s';
-
                         layer.appendChild(stickyText);
 
                         setTimeout(() => {
